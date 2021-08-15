f95a186c6ae8f2e8c7e132698ba389cc694a28a3
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-15 21:36:09 +0900

    userモデルのテストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    name                  {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"野田"}
+    first_name            {"大貴"}
+    last_name_kana        {"ノダ"}
+    first_name_kana       {"タイキ"}
+    birth_day             {"20190101"}
+  end
+end
