89492c88420b3f4b2be66a80dccd792615b10a15
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-18 01:07:03 +0900

    createアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,12 +11,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
