9a553e16c7266f122ab60e1256cee649a5f8a8aa
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-17 23:31:40 +0900

    active hash(送料)

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
     validates :item_info
     validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :item_sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :item_shipping_fee_status_id
+    validates :item_shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :item_prefecture_id
     validates :item_scheduled_delivery_id
     validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
@@ -17,5 +17,6 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
   belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
 
 end
