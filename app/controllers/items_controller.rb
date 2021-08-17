6b84ca470ceeafcf87f5d32afe0d6dfc3f6aabd9
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-17 21:10:44 +0900

    ストロングパラメーター記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
 
   private
 
+  def message_params
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
