c6d7c41989ab6493d0d1479c169b5b9101325146
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-17 21:01:03 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
+  def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
