7c94e3bc82cf9199e80187711a57d406e66ee493
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-17 20:00:26 +0900

    itemテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_093722) do
+ActiveRecord::Schema.define(version: 2021_08_17_105542) do
+
+  create_table "items", charset: "utf8mb4", force: :cascade do |t|
+    t.string "item_name"
+    t.text "item_info"
+    t.integer "item_category_id"
+    t.integer "item_sales_status_id"
+    t.integer "item_shipping_fee_status_id"
+    t.integer "item_prefecture_id"
+    t.integer "item_scheduled_delivery_id"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_08_15_093722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
