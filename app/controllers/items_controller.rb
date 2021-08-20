12a9c1d0ab573a6d99f92757380ca644d4239ee8
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-20 13:44:19 +0900

    変数の設定の変更
    item→@itemとすることでエラーメッセージが表示される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to action: :show
     else
       render action: :edit
