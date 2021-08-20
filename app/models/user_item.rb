66f98c00a408c51d4410c33d458396973fc5ccb2
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-20 23:21:11 +0900

    Formオブジェクトを作成

---
+++ app/models/user_item.rb
@@ -0,0 +1,20 @@
+class ItemUser < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :token, :postal_code, :item_prefecture_id, :city_name, :block_name, :building_name, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :token, :city_name, :block_name
+    validates :prefecture_id, numericality: { other_than: 1 }
+    # validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    
+    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
+      validates :postal_code, length: { is: 8 } 
+    end
+  end
+
+    def save
+      Address.create(postal_code: postal_code, item_prefecture_id: item_prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number)
+      Order.create(user_id: user_id, item_id: item_id)
+    end
+end
