ecc0bca2b3da7670e876b18b99a6d91e243ed6b8
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-20 14:47:03 +0900

    editアクションの編集
    他ユーザーが自身の商品の編集を防ぐ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
