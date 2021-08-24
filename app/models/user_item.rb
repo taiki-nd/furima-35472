6fabf89b9258f40144afec96e8ba4de82c7e3314
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-24 22:03:43 +0900

    バリデーションの修正

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -7,7 +7,7 @@ class UserItem
     validates :addresses
     validates :item_prefecture_id, numericality: { other_than: 1 }
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :phone_number, format: { with: /\A\d{10, 11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
   end
 
   def save
