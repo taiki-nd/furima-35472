c1cf2cb0963138a25716094ffa41bf31da290a50
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-24 16:19:15 +0900

    バリデーションの修正

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,16 +1,13 @@
 class UserItem
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id
 
   with_options presence: true do
-    validates :token, :city_name, :addresses
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :addresses
+    validates :item_prefecture_id, numericality: { other_than: 1 }
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :phone_number, format: { with: /\A\d{11}\z/ }
-    
-    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
-      validates :postal_code, length: { is: 8 } 
-    end
   end
 
   def save
