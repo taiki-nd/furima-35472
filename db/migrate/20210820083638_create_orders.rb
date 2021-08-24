eb9e204819032e20c565927fd8659dda32a71e51
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-24 14:25:53 +0900

    マイグレーションファイルの修正

--- db/migrate/20210820083638_create_orders.rb
+++ db/migrate/20210820083638_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.references :item
-      t.references :user
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
