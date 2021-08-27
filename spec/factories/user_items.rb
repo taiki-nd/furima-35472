26d3fd4d2ebf3602ea9fd2722d85321309de6405
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-27 14:54:11 +0900

    修正

--- spec/factories/user_items.rb
+++ spec/factories/user_items.rb
@@ -7,8 +7,6 @@ FactoryBot.define do
     building { '東京ビル' }
     phone_number { '09011112222' }
     token {"tok_abcdefghijk00000000000000000"}
-    user_id { '1' }
-    item_id { '1' }
   end
 end
 
--- spec/models/user_item_spec.rb
+++ spec/models/user_item_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe UserItem, type: :model do
   describe '商品購入情報の保存' do
     before do
-      @user_item = FactoryBot.build(:user_item)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @user_item = FactoryBot.build(:user_item, user_id: user.id, item_id: item.id)
+      sleep 0.1
     end
 
     context '内容に問題ない場合' do
